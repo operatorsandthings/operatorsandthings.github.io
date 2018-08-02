a0b5f460dfe94271e1b92566a1578ba425c72273
operatorsandthings <noteroclada@gmail.com>
2018-08-02 14:00:10 +0500

    initial commit

---
+++ scripts/nonsense.js
@@ -0,0 +1,42 @@
+
+
+
+
+$(document).ready(function() {
+	
+	$("#message_begin").fadeIn(3000);
+	$("#area_main").fadeIn(3000);
+	$("#command_line").fadeIn(3000);
+	
+	var counter = 0;
+	var m_length = $(".message").length;
+	
+	$("form").submit(function() {
+		//var input = $("#command_line").val();
+		
+		
+		   if(counter < m_length) {
+			
+			    $(".message").eq(counter).clone().insertBefore("#placeholder").fadeIn(2000);
+				
+					if(counter == 4 || counter == 9 || counter == 14 || counter == 17 || counter == 24) {	
+						//magic numbers, do not touch
+						$("#linebreak").clone().insertBefore("#placeholder").fadeIn(3000);
+						$(".message_r").eq(Math.floor(Math.random()*$(".message_r").length)).clone().insertBefore("#placeholder").fadeIn(3500);
+						$("#linebreak").clone().insertBefore("#placeholder").fadeIn(4000);
+					}
+			
+			    counter++;
+				
+			} else {
+				$("#message_final").clone().insertBefore("#placeholder").fadeIn(3000);
+				$(".message_r").eq(Math.floor(Math.random()*$(".message_r").length)).clone().insertBefore("#placeholder").fadeIn(3000);
+			}
+			
+			
+			
+		$("#console").scrollTop($("#console")[0].scrollHeight);
+		$("#command_line").val("");
+	});
+	
+});
